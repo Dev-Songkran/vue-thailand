bf66d25e71c25f0cc3b387ef17a8ced12818dbcf
Dev-Songkran <cronozen@gmail.com>
2020-06-26 16:17:57 +0700

    fixed bugs

--- src/VueThailand.vue
+++ src/VueThailand.vue
@@ -114,7 +114,7 @@ export default {
       if (!item) {
         item = this.suggestions[this.cursor];
       }
-
+      this.selectAddress = item;
       if (item[this.type]) {
         this.changeValue(item[this.type]);
       }
